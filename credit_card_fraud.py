c224bee88c49f951b70e083d09db9ced1508583c
Fatemeh Naqavi <naqavi@UG.KTH.SE>
2022-05-03 17:20:34 +0200

    added scaler

--- credit_card_fraud.py
+++ credit_card_fraud.py
@@ -16,12 +16,16 @@ from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('creditcard.csv')
 
-X = df.iloc[:,1:-1]
+X = df.iloc[:,0:-1]
 y = df.iloc[:,-1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.1, random_state=0)
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
 
-model = LogisticRegression(solver='lbfgs')
+model = LogisticRegression(random_state=0)
 
 clf = model.fit(X_train, y_train)
 yhat = clf.predict_proba(X_test)
